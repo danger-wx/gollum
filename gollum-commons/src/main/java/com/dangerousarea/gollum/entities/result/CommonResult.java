cb47df2df3e3a6bef54eb34b5744bcf16c482863
danger-wx <382244086@qq.com>
2020-03-22 11:11:58 +0800

    test
    test git

---
+++ gollum-commons/src/main/java/com/dangerousarea/gollum/entities/result/CommonResult.java
@@ -0,0 +1,14 @@
+package com.dangerousarea.gollum.entities.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+    private Integer code;
+    private String message;
+    private T data;
+}
