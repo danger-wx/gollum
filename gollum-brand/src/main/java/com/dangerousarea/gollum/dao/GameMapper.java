c6499cb77a1ffe78f5522544b22431632349bfb2
danger-wx <382244086@qq.com>
2020-05-18 12:36:17 +0800

    package

--- gollum-brand/src/main/java/com/dangerousarea/gollum/dao/GameMapper.java
+++ gollum-brand/src/main/java/com/dangerousarea/gollum/dao/GameMapper.java
@@ -2,7 +2,9 @@ package com.dangerousarea.gollum.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dangerousarea.gollum.domain.entities.Game;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface GameMapper extends BaseMapper<Game> {
 
 }
