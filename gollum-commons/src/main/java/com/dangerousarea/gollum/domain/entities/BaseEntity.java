d742b3732cb8d637e203f94df3a48212bdcd8c5f
danger-wx <382244086@qq.com>
2020-06-15 02:15:06 +0800

    精度问题 测试

--- gollum-commons/src/main/java/com/dangerousarea/gollum/domain/entities/BaseEntity.java
+++ gollum-commons/src/main/java/com/dangerousarea/gollum/domain/entities/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.dangerousarea.gollum.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import java.util.Date;
 @Data
 public class BaseEntity {
     @ApiModelProperty(hidden = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long id;
     @ApiModelProperty(hidden = true)
     private Integer dataStatus;
